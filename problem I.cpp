6b833fb6a36c0c68d9549d740083e3cd299caee6
Hendabdelnaser <124472702+Hendabdelnaser@users.noreply.github.com>
2024-02-01 21:21:59 +0200

    problem 8

---
+++ problem I.cpp
@@ -0,0 +1,15 @@
+﻿# include <iostream>
+using namespace std;
+int main()
+{
+	long long x, y;
+	cin >> x >> y;
+	if (x >= y)
+	{
+		cout << "Yes" << endl;
+
+	}
+	else
+		cout << "No" << endl;
+	return 0;
+}
