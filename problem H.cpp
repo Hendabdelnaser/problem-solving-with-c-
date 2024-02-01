e2b82682e1f836d3912bd34b42a99b40c598be2a
Hendabdelnaser <124472702+Hendabdelnaser@users.noreply.github.com>
2024-02-01 21:24:22 +0200

    problem 10

---
+++ problem H.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double  x, y;
+    cin >> x >> y;
+    double result = x / y;
+    cout << "floor " << x << " / " << y << " = " << floor(result) << endl;
+    cout << "ceil " << x << " / " << y << " = " << ceil(result) << endl;
+    cout << "round " << x << " / " << y << " = " << round(result) << endl;
+
+
+
+
+
+
+
+}
+
+
+
+
