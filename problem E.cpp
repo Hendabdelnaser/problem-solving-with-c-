ddab47b584aa825606d67d02b739ffad4fc44c9f
Hendabdelnaser <124472702+Hendabdelnaser@users.noreply.github.com>
2024-02-01 21:17:01 +0200

    problem 5

--- problem E.cpp
+++ problem E.cpp
@@ -1,11 +1,13 @@
-﻿#include<iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
 int main()
 {
-	long long A, B, C, D, X;
-	cin >> A >> B >> C >> D;
-	X = (A * B) - (C * D);
-	cout << "Difference = " << X;
+	double r;
+	double const PI = 3.141592653;
+	cin >> r;
+
+	cout << fixed << setprecision(9) << PI * r * r <<  "\n";
 
 	return 0;
 }
