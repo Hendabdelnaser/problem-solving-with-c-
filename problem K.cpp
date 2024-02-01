4b2763968885afea6d086c0a67bf89fed4fcefb2
Hendabdelnaser <124472702+Hendabdelnaser@users.noreply.github.com>
2024-02-01 21:26:08 +0200

    problem 11

---
+++ problem K.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int  x, y, z, one, two;
+    cin >> x >> y >> z;
+    one = min(x, y);
+    two = max(x, y);
+    cout << min(one, z) << " " << max(two, z) << endl;
+
+
+
+
+
+
+
+}
+
+
+
+
