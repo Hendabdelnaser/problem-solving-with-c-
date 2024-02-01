c4e12b066f530d0181c4a4fb0d88184dcdfc8661
Hendabdelnaser <124472702+Hendabdelnaser@users.noreply.github.com>
2024-02-01 21:40:11 +0200

    Add files via upload

---
+++ sheet 1/problem L.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string x, y, a, b;
+    cin >> x >> y;
+    cin >> a >> b;
+    if (y == b)
+    {
+        cout << "ARE Brothers" << endl;
+    }
+    else
+        cout << "NOT" << endl;
+
+
+
+
+}
+
+
+
+
